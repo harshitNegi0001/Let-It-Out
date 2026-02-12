6bcef3e313c0bdb9f0901c1b55b2816f87fc88a2
Harshit Singh Negi <realmiphone0001@gmail.com>
2026-02-12 16:10:12 +0530

    .

--- Backend/config/nodemailer.js
+++ Backend/config/nodemailer.js
@@ -8,9 +8,7 @@ dotenv.config();
 export const sendEmail = async (email, subject, message) => {
     try {
         const transporter = nodemailer.createTransport({
-            host: 'smtp.gmail.com',
-            port: 587,
-            secure: false, // use STARTTLS
+            service: 'gmail', 
             auth: {
                 user: process.env.OTP_SENDER_EMAIL,
                 pass: process.env.OTP_SENDER_PASSWORD
