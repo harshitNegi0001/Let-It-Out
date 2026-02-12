2ddec5e9a3f84f13c63c9bf2d1d0351e5aef2c25
Harshit Singh Negi <realmiphone0001@gmail.com>
2026-02-12 17:25:08 +0530

    .

--- Backend/config/send_email.js
+++ Backend/config/send_email.js
@@ -2,18 +2,23 @@ import { Resend } from 'resend';
 import dotenv from 'dotenv';
 
 dotenv.config();
-const api =process.env.RESEND_API_KEY;
+const api = process.env.RESEND_API_KEY;
 
 const resend = new Resend(api);
 
 
-export const sendEmail = async(email,subject,message)=>{
+export const sendEmail = async (email, subject, message) => {
 
+    try {
+        await resend.emails.send({
+            from: process.env.OTP_SENDER_EMAIL,
+            to: email,
+            subject: subject,
+            html: message
+        })
+    } catch (err) {
+        console.log(err);
+        throw err;
+    }
 
-    await resend.emails.send({
-        from:process.env.OTP_SENDER_EMAIL,
-        to:email,
-        subject:subject,
-        html:message
-    })
 }
